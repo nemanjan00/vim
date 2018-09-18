cb2ed05c42120d9dc783b13632671e58a1954d55
nemanjan00 <nemanjan00@gmail.com>
2018-09-18 14:00:56 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
-cd ../
-mv ./vim ./.vim
+#!/bin/bash
+
+WORKING_DIR=$(pwd)
+
+cd ~
+mv $WORKING_DIR ./.vim
+
 cd ./.vim
 
 ln -fs ~/.vim/vimrc ../.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -s ~/.vim ~/.config//nvim
+ln -s ~/.vim ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 
-vim +PlugInstall +qall
+nvim +PlugInstall +qall
+
