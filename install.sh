77e949ebdc216927d52e622a70e82b1b5445c929
nemanjan00 <nemanjan00@gmail.com>
2018-11-12 01:34:55 +0100

    Fix path

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mv $WORKING_DIR ./.config/nvim
 
 cd ./.config/nvim
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s ~/.config/nvim ~/.vim
 ln -s ~/.config/nvim/init.vim ~/.vimrc
